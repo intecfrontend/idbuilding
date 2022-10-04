478faa585ea2f219d325d52d6e52bed41b099805
intecfrontend <53944321+intecfrontend@users.noreply.github.com>
2022-10-04 20:15:29 +0200

    adding fade in animation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import './App.css';
+import React, { useEffect } from "react"
 import First from "./pages/first/first"
 import Third from "./pages/third/third"
 import Fourth from "./pages/fourth/fourth"
 import Fifth from "./pages/fifth/fifth"
-
+import AOS from "aos";
+import "aos/dist/aos.css";
 // import gsap from "gsap";
 // import { ScrollTrigger } from "gsap/ScrollTrigger";
 // gsap.registerPlugin(ScrollTrigger);
 // gsap.to("#sd", { scrollTrigger: { trigger: "#sd", toggleActions: "restart pause reverse pause" }, x: 400, duration: 5 })
 function App() {
+  useEffect(() => { AOS.init({ duration: 400 }) }, []);
+  AOS.init();
   return (
     <div className="App">
       <header className="App-header">
 
-        <First />
-        <Third />
-        <Fourth />
+        <div class="snapper" data-aos="fade-up" data-aos-delay="100"
+          data-aos-easing="linear">
+          <First />
+        </div>
+        <div class="snapper" data-aos="fade-up" data-aos-delay="100"
+          data-aos-easing="linear">
+          <Third />
+        </div>
+        <div class="snapper" data-aos="fade-up" data-aos-delay="100"
+          data-aos-easing="linear">
+          <Fourth />
+        </div>
+
+
         <Fifth />
 
       </header>
